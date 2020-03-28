d45a2223e9d4d99659d43b4248d66afef024ea9c
Eduardo Silva <edubrito87@gmail.com>
2020-03-28 17:53:46 -0300

    Testes e validações

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,25 @@
+const request = require ('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+
+describe ('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+    it('Shoud be able to create a new ONG', async () => {
+        const response = await request(app)
+        .post('/ongs')
+        .send({
+            name: "APAD2",
+            email:"contato@apad.com",
+            whatsapp: "2155555555",
+            city: "Rio do Sul",
+            uf: "SC"
+        })
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
+    });
+});
